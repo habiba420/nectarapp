f0f440ef5c748768a911291726ee7ea8e34520c5
TanzumSamrin <tanzumsamrin@gmail.com>
2025-04-14 23:51:44 +0600

    samrin

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
     DevicePreview(
